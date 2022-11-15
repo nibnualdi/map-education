a5890320abefdf92267f0a62f289d7043b34b762
nibnualdi <nibnualdi@gmail.com>
2022-11-15 19:23:29 +0700

    change tab to be 2

--- backend/connectDB.js
+++ backend/connectDB.js
@@ -1,21 +1,20 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 // let polygons;
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'web_edu'
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "web_edu",
 });
 connection.connect((err) => {
   if (err) throw err;
-  console.log('Connected!');
+  console.log("connected to database");
 });
 
 const getPolygons = (getFunc) => {
-  connection.query('SELECT * FROM polygons', getFunc);
-}
-
+  connection.query("SELECT * FROM polygons", getFunc);
+};
 
 module.exports = { getPolygons };
